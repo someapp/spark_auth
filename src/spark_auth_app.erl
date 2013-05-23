16e33bb658db46ac0ae75cc97ccfab78afa1e126
root <root@talkserver.(none)>
2013-05-23 19:15:13 -0400

    fix name mismatch

--- src/spark_auth_app.erl
+++ src/spark_auth_app.erl
@@ -1,4 +1,4 @@
--module(myapp_app).
+-module(spark_auth_app).
 
 -behaviour(application).
 
@@ -10,7 +10,7 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    myapp_sup:start_link().
+    spark_auth_sup:start_link().
 
 stop(_State) ->
     ok.
